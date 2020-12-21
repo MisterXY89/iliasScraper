fb40276e3f5d1aaa3d95c1548dc63698678cb776
Tilman Kerl <tilman.kerl@uni-konstanz.de>
2020-12-21 12:41:48 +0100

    better windows support + outsource extension determine

--- iliasScraper/download_handler.py
+++ iliasScraper/download_handler.py
@@ -1,26 +1,23 @@
 
 import os
 import fleep
+import platform
 
 class DownloadHandler:
     """
     docstring for RequestHandler.
     """
 
-    def __init__(self, request_handler):
+    def __init__(self, request_handler, target_dir):
         self.request_handler = request_handler
+        self.target_dir = target_dir
+        if platform.system() == "windows":
+            self.PATH_DELIMITER = "\\"
+        else:
+            self.PATH_DELIMITER = "/"
 
-    def download(self, file_dict, path):
-        path = os.getcwd() + "/" + path
-        if len(path) == 0:
-            path = "test"
-        self._verify_path(path)
-        url = file_dict["url"]
-        file_name = file_dict["file_name"]
-        file = self.request_handler.get_file(url)
-        file_loc = path+"/"+file_name
-        open(file_loc, "wb").write(file.content)
 
+    def _determine_extension(self, file_loc):
         with open(file_loc, "rb") as file:
             info = fleep.get(file.read(256))
         if not info.extension:
@@ -32,13 +29,38 @@ class DownloadHandler:
             extension = "pdf"
         elif extension == "pages":
             extension = "zip"
+        return extension
 
+    def download(self, file_dict, file_path, ignore_endings):
+        path = ""
+        if not self.target_dir[:-1] == self.PATH_DELIMITER:
+            self.target_dir += self.PATH_DELIMITER
+
+        # relative path in script execution dir
+        if self.target_dir == self.PATH_DELIMITER:
+            path += os.getcwd()
+
+        # else: an absolute path has been set and will be used
+        path += self.target_dir + file_path
+
+        # create path if does not exist
+        self._verify_path(path)
+        url = file_dict["url"]
+        file_name = file_dict["file_name"]
+        file = self.request_handler.get_file(url)
+        file_loc = path+self.PATH_DELIMITER+file_name
+        # save file without file-ending
+        open(file_loc, "wb").write(file.content)
+        # set file-ending
+        extension = self._determine_extension(file_loc)
+        # if extension in ignore_endings:
+        #     # delete file
         os.rename(r""+file_loc,r""+file_loc+"."+extension)
 
     def _verify_path(self, path):
-        folder_list = path.split("/")
+        folder_list = path.split(self.PATH_DELIMITER)
         for i in range(0, len(folder_list)):
-            sub_path = "/"+"/".join(folder_list[1:i+3])
+            sub_path = self.PATH_DELIMITER+self.PATH_DELIMITER.join(folder_list[1:i+3])
             if not os.path.exists(sub_path):
                 # todo check windows
                 original_umask = os.umask(000)
