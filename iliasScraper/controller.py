5df57c373bb37d83a73360e9d303e6a158c6a883
Tilman Kerl <tilman.kerl@uni-konstanz.de>
2021-01-05 00:30:21 +0100

    add version check

--- iliasScraper/controller.py
+++ iliasScraper/controller.py
@@ -1,5 +1,5 @@
 
-import pprint
+import os
 from tqdm import tqdm
 from colorama import Fore, Back, Style
 
@@ -8,7 +8,6 @@ from .file_parser import FileParser
 from .request_handler import RequestHandler
 from .download_handler import DownloadHandler
 
-pp = pprint.PrettyPrinter(indent=4)
 
 class Controller:
     """
@@ -16,6 +15,8 @@ class Controller:
     """
 
     def __init__(self, username, target_dir, ignore):
+        self.request_handler = RequestHandler()
+        self._check_version()
         self.username = username
         self.target_dir = target_dir
         self.ignore_endings = ignore
@@ -24,11 +25,26 @@ class Controller:
         self.test_file1 = "https://ilias.uni-konstanz.de/ilias/goto_ilias_uni_file_1078407_download.html"
         self.test_file2 = "https://ilias.uni-konstanz.de/ilias/goto_ilias_uni_file_1091580_download.html"
 
+    def _check_version(self):
+        version_path = os.path.join(os.path.dirname(__file__),"VERSION")
+        with open(version_path) as f:
+            line = f.readline()
+            local_version = line.replace("\n", "")
+        remote_version = self.request_handler.get_version()
+        if remote_version != local_version:
+            print(Fore.RED + Style.BRIGHT + Back.BLACK \
+            + f"You are using version {local_version}, however version {remote_version} is available.\n" \
+            + "THIS VERSION IS DEPRECATED AND POSSIBLY UNSTABLE.\n" \
+            + "PLEASE UPDATE VIA:\n" \
+            + Style.NORMAL + Fore.BLACK + Back.WHITE \
+            + "pip install iliasScraper --upgrade" + Style.RESET_ALL \
+            + "\n"+(50*"-")+"\n")
+
     def collect(self, url):
         file_dict = {}
         print(">> Getting all folder urls...")
         all_urls = self.file_parser.get_all_urls(url)#.append(url)
-        all_urls.append(url)        
+        all_urls.append(url)
         print(Style.RESET_ALL)
         for link in all_urls:
             soup = self.request_handler.get_soup(link)
@@ -38,7 +54,7 @@ class Controller:
 
     def init_controller(self):
         self.session = self.login_handler.login()
-        self.request_handler = RequestHandler(self.session)
+        self.request_handler.init_session(self.session)
         self.file_parser = FileParser(self.request_handler)
         self.download_handler = DownloadHandler(self.request_handler, self.target_dir)
 
@@ -59,7 +75,7 @@ class Controller:
         self.download_handler.download({"url":self.test_file2, "file_name":"testfile"}, "")
 
 
-# controller = Controller(controller.test_course_url,
-# name="Bachelor-Seminar Datenanalyse und Visualisierung | Master's Seminar Data Analysis and Visualization")
-# controller.download()
+# controller = Controller("tilman.kerl", "", ignore="None")
+# controller.init_controller()
+# controller.download(controller.test_course_url, "bsc")
 # controller.test()
--- iliasScraper/request_handler.py
+++ iliasScraper/request_handler.py
@@ -7,11 +7,15 @@ class RequestHandler:
     docstring for RequestHandler.
     """
 
-    def __init__(self, session):
+    def __init__(self):
         self.parser = "html.parser"
+
+    def init_session(self, session):
         self.session = session
 
     def get_soup(self, url):
+        if not self.session:
+            return False
         try:
             response = self.session.get(url)
         except Exception as err:
@@ -29,6 +33,8 @@ class RequestHandler:
             return None
 
     def get_file(self, url):
+        if not self.session:
+            return False
         try:
             response = self.session.get(url, allow_redirects=True)
         except Exception as err:
@@ -38,6 +44,20 @@ class RequestHandler:
             return False
         if response.status_code == 200:
             return response
+        else:
+            print(f'Something went wrong. Got the following response code: {response.status_code}')
+            return None
+
+    def get_version(self):
+        try:
+            response = requests.get("https://raw.githubusercontent.com/MisterXY89/iliasScraper/main/iliasScraper/VERSION")
+        except Exception as err:
+            print(f"Error: {err}")
+            print(f"With the following: {url=}")
+            raise err
+            return False
+        if response.status_code == 200:
+            return response.text.replace("\n", "")
         else:
             print(f'Something went wrong. Got the following response code: {response.status_code}')
             return None
