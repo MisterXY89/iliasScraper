add6a1ae7b2f8e724ec292998a1fcb8f8bba9776
Tilman Kerl <tilman.kerl@uni-konstanz.de>
2021-03-28 22:26:10 +0200

    platform dependent delimiter

--- iliasScraper/config.py
+++ iliasScraper/config.py
@@ -1,4 +1,11 @@
 
+import platform
 
 SCRAPER_NAME = "ilias-scraper"
 LOGIN_POST_URL = "https://ilias.uni-konstanz.de/ilias/ilias.php?lang=de&client_id=ilias_uni&cmd=post&cmdClass=ilstartupgui&cmdNode=y1&baseClass=ilStartUpGUI&rtoken="
+
+
+if platform.system() == "windows":
+    PATH_DELIMITER = "\\"
+else:
+    PATH_DELIMITER = "/"
